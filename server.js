40498c657f763f7d5b114fbb5ddcf3a7365270d7
Pavuluri Yoganand <yoganand8944@gmail.com>
2026-06-22 11:32:54 +0530

    Add Render port support

--- server.js
+++ server.js
@@ -97,6 +97,8 @@ app.get("/products", async (req, res) => {
     });
   }
 });
- app.listen(5000, () => {
-  console.log("Server running on port 5000");
+ const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
